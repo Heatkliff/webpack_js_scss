7bd175f2dd3712b4701b673bb89c87e7c26c74db
Mykyta Koriak <mykyta.koriak@onix-systems.com>
2021-04-13 19:19:16 +0300

    Ready build

---
+++ readmy.md
@@ -0,0 +1,34 @@
+# Webpack build with SCSS and JS
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/get-npm) to init this build.
+
+```bash
+npm install
+```
+
+Or use the package manager [yarn](https://yarnpkg.com/getting-started/install) to init with this build. ([yarn](https://yarnpkg.com/getting-started/install) doesn't work without [npm](https://www.npmjs.com/get-npm))
+
+```bash
+yarn
+```
+
+## Usage
+
+```bash
+yarn dev # for development mode
+yarn build # for development production
+yarn watch # for start watcher
+```
+
+or
+
+```bash
+npm run dev # for development mode
+npm run build # for development production
+npm run watch # for start watcher
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        app: [
+            './src/styles/style.scss',
+            './src/scripts/index.js',
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'scripts/[name].bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    }
+}
